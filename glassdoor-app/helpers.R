bae7714590e8a2f642791595801d2bfebc0db060
Andrea <andreamarsha@gmail.com>
2019-05-09 17:22:48 +0100

    Add function to plot rating distribution

--- glassdoor-app/helpers.R
+++ glassdoor-app/helpers.R
@@ -1,6 +1,44 @@
-filter_reviews <- function(data, company1, company2, start_date, end_date){
-    data %>% 
+filter_reviews <- function(input_data, selected_company, selected_country, start_date, end_date){
+    df <- input_data %>% 
         mutate(date = as.Date(date)) %>% 
-        filter((company == company1) | (company == company2)) %>% 
-        filter(date >= start_date)
+        dplyr::filter(company == selected_company,
+               date >= start_date, 
+               date <= end_date) %>% 
+        {if (selected_country != "All") dplyr::filter(., country == selected_country) else filter(.)}
+    
+    return(df)
+}
+
+plot_rating_distribution <- function(input_data, rating, fill_color){
+
+    df <- input_data %>%
+        dplyr::filter(!is.na(input_data[[rating]]))
+
+    avg_rating <- round(mean(df[[rating]]), 2)
+
+    df[[rating]] <- factor(floor(df[[rating]]), levels = c(1, 2, 3, 4, 5))
+
+    ggplot(df,
+           aes(x = df[[rating]])) +
+        geom_bar(width = 0.7, fill = fill_color) +
+        scale_x_discrete(drop = FALSE) +
+        scale_y_continuous(breaks = scales::pretty_breaks()) +
+        coord_flip() +
+        labs(x = "Score",
+             y = "Count",
+             title = case_when(
+                 grepl("overall", rating) ~ "Overall",
+                 grepl("career", rating) ~ "Career Opportunities",
+                 grepl("comp", rating) ~ "Compensation and Benefits",
+                 grepl("culture", rating) ~ "Culture and Values",
+                 grepl("mgmt", rating) ~ "Senior Management",
+                 grepl("balance", rating) ~ "Work-Life Balance"
+             ),
+             subtitle = paste("Average:", avg_rating)
+        ) +
+        theme_minimal() +
+        theme(plot.title = element_text(size = 16, face = "bold", color = "dimgrey"),
+              plot.subtitle = element_text(size = 12, color = "dimgrey"),
+              axis.title = element_text(size = 12, color = "dimgray"),
+              axis.text = element_text(size = 12, color = "dimgray"))
 }
