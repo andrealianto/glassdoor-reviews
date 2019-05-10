49021fb03c2746ee18267d2d578ec5739c705eb0
Andrea <andreamarsha@gmail.com>
2019-05-10 10:49:18 +0100

    Add function to plot rating trend

--- glassdoor-app/helpers.R
+++ glassdoor-app/helpers.R
@@ -42,3 +42,33 @@ plot_rating_distribution <- function(input_data, rating, fill_color){
               axis.title = element_text(size = 12, color = "dimgray"),
               axis.text = element_text(size = 12, color = "dimgray"))
 }
+
+plot_rating_trend <- function(input_data, rating){
+    ggplot(input_data, aes(x = input_data$year, y = input_data[[rating]], color = company)) +
+        geom_line(size = 1.2) +
+        geom_point(size = 2) +
+        scale_x_continuous(breaks = seq(2008, 2019)) +
+        scale_y_continuous(limits = c(1, 5)) +
+        scale_color_manual(name = "Company",
+                           values = c("#ef8a62", "#67a9cf")) +
+        labs(x = "Year",
+             y = "Average Score",
+             title = case_when(
+                 grepl("overall", rating) ~ "Overall",
+                 grepl("career", rating) ~ "Career Opportunities",
+                 grepl("comp", rating) ~ "Compensation and Benefits",
+                 grepl("culture", rating) ~ "Culture and Values",
+                 grepl("mgmt", rating) ~ "Senior Management",
+                 grepl("balance", rating) ~ "Work-Life Balance"
+             )
+        ) +
+        theme_minimal() +
+        theme(plot.title = element_text(size = 20, face = "bold", color = "dimgrey"),
+              axis.text = element_text(size = 14, color = "dimgray"),
+              axis.title = element_text(size = 16, color = "dimgray"),
+              legend.text = element_text(size = 16, color = "dimgray"),
+              legend.title = element_text(size = 16, color = "dimgray", face = "bold"),
+              legend.background = element_rect(fill = "white", colour = "grey"),
+              legend.justification = c(1,1),
+              legend.position = c(0.97,0.98))
+}
